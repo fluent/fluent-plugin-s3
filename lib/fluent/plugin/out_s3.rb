739aefe12fe8632d93581916c25ea1bbdfc86f8f
Zohar Arad <zohar@zohararad.com>
2013-03-22 09:13:07 +0200

    remove old gzip config after rebase from master

--- lib/fluent/plugin/out_s3.rb
+++ lib/fluent/plugin/out_s3.rb
@@ -30,7 +30,6 @@ class S3Output < Fluent::TimeSlicedOutput
   config_param :s3_endpoint, :string, :default => nil
   config_param :s3_object_key_format, :string, :default => "%{path}%{time_slice}_%{index}.%{file_extension}"
   config_param :store_as, :string, :default => "gzip"
-  config_param :gzip, :string, :default => "yes"
   config_param :auto_create_bucket, :bool, :default => true
 
   attr_reader :bucket
