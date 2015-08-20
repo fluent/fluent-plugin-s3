100b16dc9d38db3e959761b5087423cea64533e8
Kenji Okimoto <okimoto@clear-code.com>
2015-08-20 16:29:49 +0900

    Remove `s3_` prefix from option name
    See http://docs.aws.amazon.com/sdkforruby/api/Aws/S3/Client.html#initialize-instance_method

--- lib/fluent/plugin/out_s3.rb
+++ lib/fluent/plugin/out_s3.rb
@@ -80,7 +80,7 @@ module Fluent
         options[:secret_access_key] = @aws_sec_key
       end
       options[:region] = @s3_region if @s3_region
-      options[:s3_endpoint] = @s3_endpoint if @s3_endpoint
+      options[:endpoint] = @s3_endpoint if @s3_endpoint
       options[:http_proxy] = @proxy_uri if @proxy_uri
       options[:s3_server_side_encryption] = @use_server_side_encryption.to_sym if @use_server_side_encryption
 
