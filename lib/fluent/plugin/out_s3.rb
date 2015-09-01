40d3b5bea4cd770b817154cccd4ecb52a5c2ab5d
Kenji Okimoto <okimoto@clear-code.com>
2015-09-01 13:27:53 +0900

    Revert "Remove unused option `:aws_iam_retries`"
    This reverts commit 7f783aca2aa5cb77686e8b49d96fa70941119e57.
    
    For backward compatibility.

--- lib/fluent/plugin/out_s3.rb
+++ lib/fluent/plugin/out_s3.rb
@@ -38,6 +38,7 @@ module Fluent
       config_param :path, :string, :default => nil
       config_param :profile_name, :string, :default => nil
     end
+    config_param :aws_iam_retries, :integer, :default => 5
     config_param :s3_bucket, :string
     config_param :s3_region, :string, :default => ENV["AWS_REGION"] || "us-east-1"
     config_param :s3_endpoint, :string, :default => nil
